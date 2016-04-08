efb8285307ffadf448afe8c1dc1c2a105e1f164b
jack gao <nobody_gao@sina.com>
2016-04-08 13:26:57 +0800

    dev1

--- ThreadTest/src/com/test/thread/ExecutorServiceTest.java
+++ ThreadTest/src/com/test/thread/ExecutorServiceTest.java
@@ -10,7 +10,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 
-
+//add new dev branch
 public class ExecutorServiceTest {
 	
 	public static final Random rd=new Random();
