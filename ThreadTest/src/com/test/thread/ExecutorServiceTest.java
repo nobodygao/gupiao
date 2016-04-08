29ec02f80d772c21b643a5e347b5cacf67463313
jack gao <nobody_gao@sina.com>
2016-04-08 13:03:45 +0800

    commited 3

--- ThreadTest/src/com/test/thread/ExecutorServiceTest.java
+++ ThreadTest/src/com/test/thread/ExecutorServiceTest.java
@@ -9,7 +9,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
 
 
 public class ExecutorServiceTest {
--- ThreadTest/src/com/test/thread/TestBase64.java
+++ ThreadTest/src/com/test/thread/TestBase64.java
@@ -8,7 +8,7 @@ public class TestBase64 {
 
 	public static void main(String[] args) throws IOException {
 		String BASIC = "YTE1ZTZhMTEtNTk5My00MTA5LWI0N2MtMzUyOGFhNzA3ZmYwOjMxNjAwNDdlLTBhNWEtNDc1OS05ZDQ1LWM5N2ZkY2E3OGU5MQ==";
-		String BASIB = "YTE1ZTZhMTEtNTk5My00MTA5LWI0N2MtMzUyOGFhNzA3ZmYwOjMxNjAwNDdlLTBhNWEtNDc1OS05ZDQ1LWM5N2ZkY2E3OGU5MQ==";
+		//String BASIB = "YTE1ZTZhMTEtNTk5My00MTA5LWI0N2MtMzUyOGFhNzA3ZmYwOjMxNjAwNDdlLTBhNWEtNDc1OS05ZDQ1LWM5N2ZkY2E3OGU5MQ==";
 		String s=new String(new BASE64Decoder().decodeBuffer(BASIC),"utf-8");
 		System.out.println(s);
 		
