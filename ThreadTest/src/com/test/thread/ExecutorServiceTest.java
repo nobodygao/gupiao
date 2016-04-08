aead00403911b0dd8f586dd282cb84c3c316697e
jack gao <nobody_gao@sina.com>
2016-04-08 12:44:29 +0800

    commited by jack gao

---
+++ ThreadTest/src/com/test/thread/ExecutorServiceTest.java
@@ -0,0 +1,96 @@
+package com.test.thread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+
+public class ExecutorServiceTest {
+	
+	public static final Random rd=new Random();
+	public static final BlockingQueue<String> queue=new LinkedBlockingQueue<String>();
+	
+    public static void main(String[] args) throws InterruptedException {  
+    	rd.setSeed(System.currentTimeMillis());
+        ExecutorService executorService = Executors.newFixedThreadPool(10);  
+        List<Future<String>> resultList = new ArrayList<Future<String>>();  
+  
+        // 创建10个任务并执行  
+        for (int i = 0; i < 30; i++) {  
+            // 使用ExecutorService执行Callable类型的任务，并将结果保存在future变量中  
+            Future<String> future = executorService.submit(new TaskWithResult(i));  
+            // 将任务执行结果存储到List中  
+            resultList.add(future);  
+        }  
+        
+         
+        // 遍历任务的结果
+        Thread monitor=new RcvThread();
+        monitor.start();
+        /*
+        System.out.println("shutdown---");
+        executorService.shutdown();
+        Future<String> future = executorService.submit(new TaskWithResult(1000));  
+        // 将任务执行结果存储到List中  
+        resultList.add(future);  
+        executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+        queue.put("quit");
+        System.out.println("all thread shutdown");
+              */
+    }  
+    
+}  
+
+
+class RcvThread extends Thread{
+
+	@Override
+	public void run() {
+		 while(true){
+	        	String msg=null;
+				try {
+					msg = ExecutorServiceTest.queue.take();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        	System.out.println(msg+System.currentTimeMillis());
+	        	if("quit".equals(msg))
+	        		return;
+	        } 
+		
+	}
+	
+}
+
+
+
+class TaskWithResult implements Callable<String> {  
+	private int id;  
+	  
+    public TaskWithResult(int id) {  
+        this.id = id;  
+    }  
+  
+    /** 
+     * 任务的具体过程，一旦任务传给ExecutorService的submit方法，则该方法自动在一个线程上执行。 
+     *  
+     * @return 
+     * @throws Exception 
+     */  
+    public String call() throws Exception {  
+        System.out.println(id+":干活！！！ " + Thread.currentThread().getName()+","+System.currentTimeMillis());  
+        Thread.sleep(ExecutorServiceTest.rd.nextInt(10000));     
+        String msg=id + ":" + Thread.currentThread().getName();
+        ExecutorServiceTest.queue.put(msg);
+        return msg;
+    }  
+} 
+
---
+++ ThreadTest/src/com/test/thread/TestBase64.java
@@ -0,0 +1,18 @@
+package com.test.thread;
+
+import java.io.IOException;
+
+import sun.misc.BASE64Decoder;
+
+public class TestBase64 {
+
+	public static void main(String[] args) throws IOException {
+		String BASIC = "YTE1ZTZhMTEtNTk5My00MTA5LWI0N2MtMzUyOGFhNzA3ZmYwOjMxNjAwNDdlLTBhNWEtNDc1OS05ZDQ1LWM5N2ZkY2E3OGU5MQ==";
+		String BASIB = "YTE1ZTZhMTEtNTk5My00MTA5LWI0N2MtMzUyOGFhNzA3ZmYwOjMxNjAwNDdlLTBhNWEtNDc1OS05ZDQ1LWM5N2ZkY2E3OGU5MQ==";
+		String s=new String(new BASE64Decoder().decodeBuffer(BASIC),"utf-8");
+		System.out.println(s);
+		
+
+	}
+
+}
